5ba44d3e7441e7db1562190d1c8ac112fb069c4e
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2025-09-26 11:09:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 
 void plain_func(int& x, int argc)
 {
-        if (x % argc == 0)
-                x += argc;
-        else
-                x += 1;
+    if (x % argc == 0)
+        x += argc;
+    else
+        x += 1;
 }
 
-int main(int argc, char** ) 
+int main(int argc, char**) 
 {
     constexpr int N = 100'000'000; 
 
@@ -20,15 +20,14 @@ int main(int argc, char** )
         void (*fp)(int&,int) = plain_func;
         auto start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < N; i++) {
-            fp(x,argc);
-
+            fp(x, argc);
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::cout << "Function pointer: "
                   << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
-                  << " nanosecs in total, " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
-                  << " ns per call  "
-                  << std::endl;
+                  << " ns total, " 
+                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
+                  << " ns/call\n";
     }
 
     // 2. Inline lambda (should inline away)
@@ -36,21 +35,21 @@ int main(int argc, char** )
         int x = 0;
         auto lambda = [](int& y, int argc) 
         { 
-                if (y % argc == 0)
-                        y += argc;
-                else
-                        y += 1;
+            if (y % argc == 0)
+                y += argc;
+            else
+                y += 1;
         };
         auto start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < N; i++) {
-            lambda(x ,argc);
+            lambda(x, argc);
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::cout << "Lambda (inline) : "
                   << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
-                  << " nanosecs in total, " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
-                  << " ns per call  "
-                  << std::endl;
+                  << " ns total, " 
+                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
+                  << " ns/call\n";
     }
 
     // 3. std::function
@@ -59,14 +58,30 @@ int main(int argc, char** )
         std::function<void(int&,int)> f = plain_func;
         auto start = std::chrono::high_resolution_clock::now();
         for (int i = 0; i < N; i++) {
-            f(x,argc);
+            f(x, argc);
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::cout << "std::function   : "
                   << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
-                  << " milliseconds in total, " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
-                  << " ns per call  "
-                  << std::endl;
+                  << " ms total, " 
+                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
+                  << " ns/call\n";
+    }
+
+    // 4. std::bind
+    {
+        int x = 0;
+        auto bound = std::bind(plain_func, std::placeholders::_1, argc);
+        auto start = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < N; i++) {
+            bound(x);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "std::bind       : "
+                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                  << " ms total, " 
+                  << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / N
+                  << " ns/call\n";
     }
 
     return 0;
